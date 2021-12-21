48878f655d8774106f0c67f33ce82a997a5eac76
Jinjing Bao <jinjingbao.z@gmail.com>
2021-12-21 15:26:09 +0000

    completed lab3

---
+++ lab3/Flik/FlikTest.java
@@ -0,0 +1,10 @@
+import org.junit.Test;
+
+public class FlikTest {
+    @Test
+    public void testFlik(){
+        int a =128;
+        int b =128;
+        System.out.println(Flik.isSameNumber(a,b));
+    }
+}
